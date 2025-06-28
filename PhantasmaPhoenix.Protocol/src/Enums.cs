72c76efabbeab831f4e687785fa6f161b21b7860
alexmpa <alexander.paukonen@gmail.com>
2025-06-28 13:01:44 -0300

    Add unit conversion and token flags

--- PhantasmaPhoenix.Protocol/src/Enums.cs
+++ PhantasmaPhoenix.Protocol/src/Enums.cs
@@ -118,3 +118,18 @@ public enum Nexus
 	Testnet,
 	Simnet
 }
+
+[Flags]
+public enum TokenFlags
+{
+	None = 0,
+	Transferable = 1 << 0,
+	Fungible = 1 << 1,
+	Finite = 1 << 2,
+	Divisible = 1 << 3,
+	Fuel = 1 << 4,
+	Stakable = 1 << 5,
+	Fiat = 1 << 6,
+	Swappable = 1 << 7,
+	Burnable = 1 << 8,
+}
