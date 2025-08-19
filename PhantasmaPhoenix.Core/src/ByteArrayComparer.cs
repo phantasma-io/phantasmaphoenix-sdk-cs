eaeac4b65a27cf114ca994fee7b2a681f5907802
alexmpa <alexander.paukonen@gmail.com>
2025-08-19 14:43:58 -0300

    Core: Update ByteArrayComparer, bump version

--- PhantasmaPhoenix.Core/src/ByteArrayComparer.cs
+++ PhantasmaPhoenix.Core/src/ByteArrayComparer.cs
@@ -1,28 +1,55 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
 
 namespace PhantasmaPhoenix.Core;
 
-public class ByteArrayComparer : IEqualityComparer<byte[]>
+public class ByteArrayComparer : IEqualityComparer<byte[]>, IEqualityComparer, IComparer<byte[]>
 {
-	public bool Equals(byte[]? left, byte[]? right)
+	int IComparer<byte[]>.Compare(byte[]? x, byte[]? y)
+	{
+		if (x == null || y == null)
+		{
+			return (x == null ? 0 : 1) - (y == null ? 0 : 1);
+		}
+		int minLength = Math.Min(x.Length, y.Length);
+		for (int i = 0; i != minLength; ++i)
+		{
+			if (x[i] < y[i])
+				return -1;
+			if (x[i] > y[i])
+				return 1;
+		}
+		if (x.Length == y.Length)
+			return 0;
+		return x.Length < y.Length ? -1 : 1;
+	}
+	bool IEqualityComparer<byte[]>.Equals(byte[]? left, byte[]? right)
 	{
 		if (left == null || right == null)
-			return left == right;
-		return left.CompareBytes(right);
-	}
-
-	public int GetHashCode(byte[] key)
-	{
-		Throw.IfNull(key, nameof(key));
-		unchecked // disable overflow, for the unlikely possibility that you
 		{
-			// are compiling with overflow-checking enabled
-			uint hash = 2166136261;
-			for (int i = 0; i < key.Length; i++)
-			{
-				hash = (16777619 * hash) ^ key[i];
-			}
-			return (int)hash;
+			return left == right;
 		}
+		return left.SequenceEqual(right);
+	}
+	bool IEqualityComparer.Equals(object? left, object? right)
+	{
+		return ((IEqualityComparer<byte[]>)this).Equals((byte[]?)left, (byte[]?)right);
+	}
+	int IEqualityComparer<byte[]>.GetHashCode(byte[] key)
+	{
+		if (key == null)
+			return 0;
+		uint hash = 2166136261;
+		for (int i = 0; i < key.Length; i++)
+		{
+			hash = (16777619 * hash) ^ key[i];
+		}
+		return (int)hash;
+	}
+	int IEqualityComparer.GetHashCode(object okey)
+	{
+		byte[]? key = (byte[]?)okey;
+		if (key == null)
+			return 0;
+		return ((IEqualityComparer<byte[]>)this).GetHashCode(key);
 	}
 }
