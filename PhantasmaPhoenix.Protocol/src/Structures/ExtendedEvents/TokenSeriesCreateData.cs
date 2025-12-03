f1ec2e95b28646a7cc049abc051e604faf399cf4
alexmpa <alexander.paukonen@gmail.com>
2025-12-03 12:24:39 -0300

    Protocol: Add extra fields to TokenSeriesCreateData, bump version

--- PhantasmaPhoenix.Protocol/src/Structures/ExtendedEvents/TokenSeriesCreateData.cs
+++ PhantasmaPhoenix.Protocol/src/Structures/ExtendedEvents/TokenSeriesCreateData.cs
@@ -2,6 +2,8 @@ namespace PhantasmaPhoenix.Protocol.ExtendedEvents;
 
 public struct TokenSeriesCreateData
 {
+	public readonly string Symbol;
+	public readonly string SeriesId;
 	public readonly uint MaxMint;
 	public readonly uint MaxSupply;
 	public readonly string Owner;
@@ -9,8 +11,10 @@ public struct TokenSeriesCreateData
 	public readonly UInt32 CarbonSeriesId;
 	public readonly Dictionary<string, string> Metadata;
 
-	public TokenSeriesCreateData(uint maxMint, uint maxSupply, string owner, UInt64 carbonTokenId, UInt32 carbonSeriesId, Dictionary<string, string> metadata)
+	public TokenSeriesCreateData(string symbol, string seriesId, uint maxMint, uint maxSupply, string owner, UInt64 carbonTokenId, UInt32 carbonSeriesId, Dictionary<string, string> metadata)
 	{
+		Symbol = symbol;
+		SeriesId = seriesId;
 		MaxMint = maxMint;
 		MaxSupply = maxSupply;
 		Owner = owner;
